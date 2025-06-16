964de0acbc9bb5ed4a5d657eee6b93207d604b7e
D-lip63 <drip6367@gmail.com>
2025-06-16 21:13:49 +0530

    Wilcoxon to MannWhitneyU

--- Modules/statistical_tests.py
+++ Modules/statistical_tests.py
@@ -1,8 +1,8 @@
-from scipy.stats import ttest_ind, wilcoxon, kruskal
+from scipy.stats import ttest_ind, mannwhitneyu, kruskal
 
 def run_tests(pre_event, post_event):
     t_stat, t_p = ttest_ind(pre_event, post_event)
-    w_stat, w_p = wilcoxon(pre_event, post_event)
+    w_stat, w_p = mannwhitneyu(pre_event, post_event,alternative='two-sided')
     k_stat, k_p = kruskal(pre_event, post_event)
-    return {'t-test': t_p, 'Wilcoxon': w_p, 'Kruskal': k_p}
+    return {'t-test': t_p, 'Mann-Whitney': w_p, 'Kruskal': k_p}
 
